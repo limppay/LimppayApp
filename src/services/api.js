04bdf4df3649e5c911ae0683ae8e5a883f139d5b
mrtaki67 <castrothiago6167@gmail.com>
2024-10-16 17:52:30 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'limppay-api-production.up.railway.app',
 });
 
 // Função para criar o usuário e enviar arquivos
