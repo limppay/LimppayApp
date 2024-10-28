4fd20bdac5120cd92eb5646423ed262e84013cd7
mrtaki67 <castrothiago6167@gmail.com>
2024-10-28 14:43:19 -0300

    update: home

--- src/componentes/home/Sobre.jsx
+++ src/componentes/home/Sobre.jsx
@@ -1,6 +1,6 @@
 export default function Sobre () {
     return (
-        <section className="flex justify-center pt-10 text-desSec text-xl *:font-semibold">
+        <section className="flex justify-center text-desSec text-xl *:font-semibold">
             <h2 className="lg:text-2xl">Por que contratar a Limppay?</h2>
         </section>
     )
