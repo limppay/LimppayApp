8ef94cffd976aedac0776c6b82c3040078f9ceeb
mrtaki67 <castrothiago6167@gmail.com>
2024-11-01 17:59:26 -0300

    update: checkout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -43,9 +43,9 @@ export default function Checkout() {
   console.log(errors)
 
   const { agendamentoData, setAgendamentoData } = useAgendamentoData();
-  const {selectedProvider} = useSelectedProvider()
-  const {selectedDates} = useSelectedDates()
-  const {selectedTimes} = useSelectedTimes()
+  const {selectedProvider, setSelectedProvider} = useSelectedProvider()
+  const {selectedDates, setSelectedDates} = useSelectedDates()
+  const {selectedTimes, setSelectedTimes} = useSelectedTimes()
   const navigate = useNavigate();
   const location = useLocation(); // Obtém a localização atual
   const [isLoadingQrCode, setIsLoadingQrCode] = useState(false)
@@ -200,33 +200,49 @@ export default function Checkout() {
   }, [setAgendamentoData]);
 
   useEffect(() => {
-    const handleRouteChange = () => {
-      // Limpa os dados apenas se sair da rota '/checkout-pagamento'
-      if (location.pathname !== '/checkout-pagamento') {
-        setAgendamentoData(null);
-        localStorage.removeItem('agendamentoData'); // Remove do localStorage
-      }
-    };
+    // Carrega os dados do localStorage quando o componente é montado
+    const storedData = localStorage.getItem('agendamentoData');
+    const selectedProvider = localStorage.getItem('selectedProvider');
+    const selectedDates = localStorage.getItem('selectedDates');
+    const selectedTimes = localStorage.getItem('selectedTimes');
+    
+    if (storedData) {
+      setAgendamentoData(JSON.parse(storedData));
+      setSelectedProvider(JSON.parse(selectedProvider));  
+      setSelectedDates(JSON.parse(selectedDates))    
+      setSelectedTimes(JSON.parse(selectedTimes))    
 
-    // Verifica se os dados do agendamento não existem, redireciona para '/contrate-online'
-    if (!agendamentoData) {
-      return navigate('/contrate-online');
+    } else {
+      navigate('/contrate-online');
     }
 
-    // Salva os dados no localStorage sempre que o estado mudar
-    localStorage.setItem('agendamentoData', JSON.stringify(agendamentoData));
+  }, [navigate, setAgendamentoData]);
 
-    // Ouve mudanças na rota para limpar dados ao sair de '/checkout-pagamento'
-    return () => {
-      handleRouteChange();
-    };
-  }, [location.pathname, agendamentoData, setAgendamentoData, navigate]);
+  useEffect(() => {
+    // Salva os dados no localStorage sempre que o estado mudar
+    if (agendamentoData) {
+      localStorage.setItem('agendamentoData', JSON.stringify(agendamentoData));
+      localStorage.setItem('selectedProvider', JSON.stringify(selectedProvider));
+      localStorage.setItem('selectedDates', JSON.stringify(selectedDates));
+      localStorage.setItem('selectedTimes', JSON.stringify(selectedTimes));
+    }
+
+
+  }, [agendamentoData]);
 
   if (!agendamentoData) {
     return null; // Não renderiza se os dados não estiverem disponíveis
   }
 
   console.log(dadosCartao)
+
+  const HandleCreditCard = () => {
+    setMetodoPagamento("credit_card")
+  }
+
+  const handlePix = () => {
+    setMetodoPagamento("pix")
+  }
 
   return (
     <>
@@ -235,35 +251,45 @@ export default function Checkout() {
       <main className="relative p-4 flex lg:justify-between lg:pl-20 lg:pr-20 justify-center gap-5">
         <div className='flex flex-col p-10  min-w-[50vh] max-w-[50vh] lg:min-w-[120vh] lg:max-w-[120vh] md:min-w-[80vh] md:max-w-[80vh] sm:min-w-[80vh] sm:max-w-[80vh] shadow-lg pt-20 rounded-xl lg:min-h-[150vh] min-h-[90vh]'>
           <div className="mb-6 flex flex-col">
-            <div className='pb-2' >
+            <div className='pb-4' >
               <h1 className='text-prim font-semibold text-lg'>Método de pagamento</h1>
             </div>
-            <div className="flex justify-between items-center mb-4">
-              <div className="flex items-center">
-                <input 
-                id="pp" 
-                type="radio" 
-                name="payment" 
-                className="mr-2" 
-                checked={metodoPagamento === 'credit_card'}
-                value="credit_card"
-                onChange={(e) => setMetodoPagamento(e.target.value)}
-                />
-                <label htmlFor="pp" className="text-prim ">cartão de crédito</label>
+            <div className='flex w-full gap-5'>
+              <div>
+                <div>
+                    <button
+                    className='p-2 border border-bord rounded-md text-prim  text-start w-full flex items-center gap-2 justify-between'
+                    onClick={HandleCreditCard}
+                    >
+                      <div className='flex items-center gap-2'>
+                        <i className='fas fa-credit-card font-semibold text-ter'></i>
+                          Cartão de crédito
+                      </div>
+
+                      {metodoPagamento === "credit_card" && (
+                        <i class="fas fa-check"></i>
+                      )}
+
+                    </button>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-between items-center mb-4">
-              <div className="flex items-center">
-                <input 
-                id="cd" 
-                type="radio" 
-                name="payment" 
-                className="mr-2" 
-                value='pix'
-                checked={metodoPagamento === 'pix'}
-                onChange={(e) => setMetodoPagamento(e.target.value)}
-                />
-                <label htmlFor="cd" className="text-prim">Pix</label>
+              <div className='w-4/12'>
+                <div className='w-12/12'>
+                  <button
+                  className='p-2 border border-bord rounded-md text-prim lg:w-6/12 w-full text-start flex gap-2 items-center justify-between'
+                  onClick={handlePix}
+                  >
+                    <div className='flex gap-2'>
+                      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+                        <path fill="#4db6ac" d="M11.9,12h-0.68l8.04-8.04c2.62-2.61,6.86-2.61,9.48,0L36.78,12H36.1c-1.6,0-3.11,0.62-4.24,1.76	l-6.8,6.77c-0.59,0.59-1.53,0.59-2.12,0l-6.8-6.77C15.01,12.62,13.5,12,11.9,12z"></path><path fill="#4db6ac" d="M36.1,36h0.68l-8.04,8.04c-2.62,2.61-6.86,2.61-9.48,0L11.22,36h0.68c1.6,0,3.11-0.62,4.24-1.76	l6.8-6.77c0.59-0.59,1.53-0.59,2.12,0l6.8,6.77C32.99,35.38,34.5,36,36.1,36z"></path><path fill="#4db6ac" d="M44.04,28.74L38.78,34H36.1c-1.07,0-2.07-0.42-2.83-1.17l-6.8-6.78c-1.36-1.36-3.58-1.36-4.94,0	l-6.8,6.78C13.97,33.58,12.97,34,11.9,34H9.22l-5.26-5.26c-2.61-2.62-2.61-6.86,0-9.48L9.22,14h2.68c1.07,0,2.07,0.42,2.83,1.17	l6.8,6.78c0.68,0.68,1.58,1.02,2.47,1.02s1.79-0.34,2.47-1.02l6.8-6.78C34.03,14.42,35.03,14,36.1,14h2.68l5.26,5.26	C46.65,21.88,46.65,26.12,44.04,28.74z"></path>
+                      </svg>
+                      Pix
+                    </div>
+                    {metodoPagamento === "pix" && (
+                        <i class="fas fa-check"></i>
+                    )}
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -417,7 +443,7 @@ export default function Checkout() {
                     </div>
                     <div className='max-w-full flex flex-col gap-2'>
                       <div className='flex justify-center items-center gap-2'>
-                        <p className='text-center text-prim font-semibold'>Ou use a chave PIX</p>
+                        <p className='text-center text-prim font-semibold'>Pix copia e cole</p>
                       </div>
                       <div className='flex items-center gap-2 max-w-full'>
                         <Snippet hideSymbol="true" 
