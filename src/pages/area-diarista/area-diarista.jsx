70546b64abd5ef281ee22418d858d65896402f2e
Thiago Castro <thiago.castro@jftecnologia.local>
2024-12-12 10:04:06 -0300

    update: maps

--- src/pages/area-diarista/area-diarista.jsx
+++ src/pages/area-diarista/area-diarista.jsx
@@ -1064,7 +1064,6 @@ const AreaDiarista = () => {
                                                                                         year: 'numeric'
                                                                                     })}</p>
                                                                                     <div className='text-justify flex flex-col gap-2'>
-                                                                                        <p><b>Endereço:</b> {agendamento?.enderecoCliente}</p>
 
                                                                                         <p><b>Prestador:</b> {agendamento?.user?.name}</p>
 
@@ -1079,6 +1078,24 @@ const AreaDiarista = () => {
                                                                                             month: 'long',
                                                                                             year: 'numeric'
                                                                                         })}</p>
+                                                                                        <div className='flex flex-col gap-2 max-w-[100vh]'>
+                                                                                            <p><b>Endereço:</b> {agendamento?.enderecoCliente}</p>
+                                                                                            <a 
+                                                                                                href={`https://www.google.com/maps/place/${encodeURIComponent(agendamento?.enderecoCliente)}`} 
+                                                                                                target="_blank" 
+                                                                                                rel="noopener noreferrer"
+                                                                                            >
+                                                                                                <Button className='w-full bg-sec text-white'>
+                                                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+                                                                                                    </svg>
+
+                                                                                                    Abrir com o Google Maps
+
+                                                                                                </Button>
+                                                                                            </a>
+
+                                                                                        </div>
 
                                                                                     </div> 
 
