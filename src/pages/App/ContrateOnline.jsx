be53b7525badbf80f19b0ab859a8d4446576bb49
Thiago Castro <thiago.castro@jftecnologia.local>
2025-01-06 13:16:25 -0300

    update: avarage stars

--- src/pages/App/ContrateOnline.jsx
+++ src/pages/App/ContrateOnline.jsx
@@ -1161,26 +1161,24 @@ export default function ContrateOnline() {
                                                                             
                                                                             >
                                                                                 <div className='flex items-center gap-2'>
-                                                                                    <span
-                                                                                        className={`text-4xl text-des`}
-                                                                                    >
-                                                                                        ★
-                                                                                    </span>
-                                                                                    <span>
-                                                                                        {calcularMediaStars(prestador?.Review)}
-                                                                                    </span>
-
-
-                                                                                </div>
-                                                                                <div className=''>
                                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                                                                                     </svg>
-                                                                                    <span>
+                                                                                    <div className='flex items-center gap-1'>
+                                                                                        <span
+                                                                                            className={`text-3xl text-des`}
+                                                                                        >
+                                                                                            ★
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            {calcularMediaStars(prestador?.Review)}
+                                                                                        </span>
+                                                                                    
+                                                                                    </div>
 
-                                                                                    </span>
 
                                                                                 </div>
+                                                                                
 
 
                                                                                 
