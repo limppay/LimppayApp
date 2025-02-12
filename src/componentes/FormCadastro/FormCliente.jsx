c0a162af77ac67d2efd4fdcdbd665711c8315970
mrtaki67 <castrothiago6167@gmail.com>
2025-02-12 12:50:54 -0400

    update: view password

--- src/componentes/FormCadastro/FormCliente.jsx
+++ src/componentes/FormCadastro/FormCliente.jsx
@@ -21,6 +21,8 @@ import {  Modal,   ModalContent,   ModalHeader,   ModalBody,   ModalFooter, useD
 
 export default function FormCliente() {
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    
     // schema de validações do form
     const schema = yup.object({
         // scheam do prisma na API
@@ -733,13 +735,35 @@ export default function FormCliente() {
             </div>
             <div className="mt-4 p-9 pt-0 pb-0 flex flex-col">
                 <label htmlFor="password" className="text-prim">Senha</label>
-                <input 
-                className="border rounded-md border-bord p-3 pt-2 pb-2 focus:outline-prim text-ter "
-                id="password" 
-                type="password"
-                placeholder="" 
-                {...register("senha")}
-                />
+                <div className="relative">
+                    <input 
+                    className="w-full border rounded-md border-bord p-3 pt-2 pb-2 focus:outline-prim text-ter "
+                    id="password" 
+                    type={showPassword ? "text" : "password"}
+                    placeholder="" 
+                    {...register("senha")}
+                    />
+
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute inset-y-0 right-2 flex items-center text-ter"
+                    >
+                        {
+                            showPassword ? 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>
+                                :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+
+                        }
+                    </button>
+
+                </div>
                 {errors.senha && 
                 <span className="text-error opacity-75">{errors.senha?.message}</span>}
             </div>
@@ -747,13 +771,35 @@ export default function FormCliente() {
 
             <div className="mt-4 p-9 pt-0 pb-0 flex flex-col">
                 <label htmlFor="confirmPassword" className="text-prim">Confirmar Senha</label>
-                <input 
-                className="border rounded-md border-bord p-3 pt-2 pb-2 focus:outline-prim text-ter "
-                id="confirmPassword" 
-                type="password"
-                {...register('confirmarSenha')}
-                placeholder=""
-                />
+                <div className="relative">
+                    <input 
+                    className="w-full border rounded-md border-bord p-3 pt-2 pb-2 focus:outline-prim text-ter "
+                    id="confirmPassword" 
+                    type={showPassword ? "text" : "password"}
+                    {...register('confirmarSenha')}
+                    placeholder=""
+                    />
+
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute inset-y-0 right-2 flex items-center text-ter"
+                    >
+                        {
+                            showPassword ? 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>
+                                :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+
+                        }
+                    </button>
+
+                </div>
                 {errors.confirmarSenha && 
                 <span className="text-error opacity-75">{errors.confirmarSenha?.message}</span>}
             </div>
